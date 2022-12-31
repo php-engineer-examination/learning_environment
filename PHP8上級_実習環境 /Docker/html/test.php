2f5d359183f11d22146e3cbdbba450115b6d27b4
mikumo <34376505+3rd-cloud@users.noreply.github.com>
2022-12-31 22:44:30 +0900

    Add: PHP8上級 Docker 実習環境

---
+++ PHP8上級_実習環境 /Docker/html/test.php
@@ -0,0 +1,22 @@
+<html>
+
+<head>
+
+<title>test</title>
+
+</head>
+
+<body>
+
+<p>このページはコンテナが実行できているか確認するテストファイルです。<br />
+必要がない場合は削除しても構いません。</p>
+
+<h2>phpMyAdmin</h2>
+<p><a href="http://localhost:8080/">phpMyAdmin へリンク</a></p>
+
+<h2>phpinfo()</h2>
+<?php phpinfo(); ?>
+
+</body>
+
+</html>
